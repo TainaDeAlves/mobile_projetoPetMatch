a256c79ae97ceb98e2bdd5e1f71c8bd0b2bc65e6
Tainá Souza <tainadesouzalves@gmail.com>
2024-12-02 22:09:41 -0300

    terminei o menu

--- app_projetopetmatch/lib/home.dart
+++ app_projetopetmatch/lib/home.dart
@@ -31,30 +31,131 @@ class _HomePageState extends State<HomePage> {
           child: ListView(
             children: [
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                children: [Image.asset(
-                  'assets/logo3.png',
-                  width: 205,
-                  height: 100,
-                ),
-                ]
-              ),
+                padding: const EdgeInsets.only(bottom: 5, top: 50, left: 25),
+                child:
+                    Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+                  Image.asset(
+                    'assets/logo3.png',
+                    width: 205,
+                  ),
+                ]),
               ),
               const Padding(
-                padding: EdgeInsets.all(35.0),
+                padding: EdgeInsets.only(top: 25, left: 35),
                 child: Text(
                   "Olá, seja bem-vinda",
                   style: TextStyle(fontSize: 22, fontWeight: FontWeight.w400),
                 ),
-
-                
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 5, left: 20),
+                child: Column(
+                  children: [
+                    ListTile(
+                      title: Text(
+                        "Pagina inicial",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(
+                        "Categoria",
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xffcd9cfa)),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(
+                        "Adote",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(
+                        "Contribua",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(
+                        " Sobre nos",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(
+                        "Contatos",
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xffcd9cfa)),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(
+                        "(14) 9999-9999",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(
+                        "contato@petmatch.com",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(
+                        "Endereço; 00",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(
+                        "Faq",
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xffcd9cfa)),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(
+                        "Politica de privacidade",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(
+                        "Redes Sociais",
+                        style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xffcd9cfa)),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left:25),
+                      child: Row(
+                        children: [
+                          Icon(Icons.label_important_outline),
+                          Icon(Icons.label_important_outline),
+                          Icon(Icons.label_important_outline),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               )
             ],
           ),
         ),
       ),
+      
+      
+
+      
     );
   }
 }
